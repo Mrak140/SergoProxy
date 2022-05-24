ed7ae3a5ed55174df4213878136a38da5903cc20
Mrak140 <60341685+Mrak140@users.noreply.github.com>
2022-05-24 20:20:03 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,54 @@
+
+    const followCursor = () => { // объявляем функцию followCursor
+      const el = document.querySelector('.follow-cursor') // ищем элемент, который будет следовать за курсором
+  
+      window.addEventListener('mousemove', e => { // при движении курсора
+        const target = e.target // определяем, где находится курсор
+        if (!target) return
+  
+        if (target.closest('a')) { // если курсор наведён на ссылку
+          el.classList.add('follow-cursor_active') // элементу добавляем активный класс
+        } else { // иначе
+          el.classList.remove('follow-cursor_active') // удаляем активный класс
+        }
+  
+        el.style.left = e.pageX + 'px' // задаём элементу позиционирование слева
+        el.style.top = e.pageY + 'px' // задаём элементу позиционирование сверху
+      })
+    }
+    followCursor();
+
+
+
+
+
+
+    
+    /* функция добавления ведущих нулей */
+    /* (если число меньше десяти, перед числом добавляем ноль) */
+    function zero_first_format(value)
+    {
+        if (value < 10)
+        {
+            value='0'+value;
+        }
+        return value;
+    }
+
+    /* функция получения текущей даты и времени */
+    function date_time()
+    {
+        var current_datetime = new Date();
+        var day = zero_first_format(current_datetime.getDate());
+        var month = zero_first_format(current_datetime.getMonth()+1);
+        var year = current_datetime.getFullYear();
+        var hours = zero_first_format(current_datetime.getHours());
+        var minutes = zero_first_format(current_datetime.getMinutes());
+        var seconds = zero_first_format(current_datetime.getSeconds());
+
+        return day+"."+month+"."+year+" "+hours+":"+minutes+":"+seconds;
+    }
+
+    setInterval(function () {
+        document.querySelector(".jopa").innerHTML = date_time();
+    }, 1000);
